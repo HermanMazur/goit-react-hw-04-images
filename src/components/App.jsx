991cc33e46fba464ff74dea7d19d468507855492
HermanMazur <zlabademazur@gmail.com>
2022-10-04 17:56:43 +0300

    app need to add the button LOAD MORE???

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Searchbar from './Searchbar/Searchbar';
 import Modal from './Modal/Modal';
@@ -8,114 +8,100 @@ import toast from 'react-hot-toast';
 import api from 'services/api';
 import { mapper } from 'helpers/mapper';
 
-export default class App extends React.Component {
-  state = {
-    pictureName: '',
-    pictureData: '',
-    pictureModal: '',
-    status: 'idle',
-    page: 1,
-    IsLoadingMore: false,
-  };
+export default function App() {
+  const [{ pictureName }, setPictureName] = useState('');
+  const [pictureData, setPictureData] = useState('');
+  const [pictureModal, setPictureModal] = useState('');
+  const [status, setStatus] = useState('');
+  const [page, setPage] = useState('');
+  const [IsLoadingMore, setIsLoadingMore] = useState(false);
+  // state = {
+  //   pictureName: '',
+  //   pictureData: '',
+  //   pictureModal: '',
+  //   status: 'idle',
+  //   page: 1,
+  //   IsLoadingMore: false,
+  // };
 
-  componentDidUpdate(prevState, prevProps) {
-    const prevSearch = prevProps.pictureName;
-    const nextSearch = this.state.pictureName;
-    const prevPage = prevProps.page;
-    const nextPage = this.state.page;
-
-    if (prevSearch !== nextSearch) {
-      this.loadPicture();
-      this.resetData();
+  // componentDidUpdate(prevState, prevProps) {
+  //   const prevSearch = prevProps.pictureName;
+  //   const nextSearch = this.state.pictureName;
+  //   const prevPage = prevProps.page;
+  //   const nextPage = this.state.page;
+  useEffect(() => {
+    if (!pictureName) {
+      return;
     }
-    if (nextPage > prevPage) {
-      this.loadPicture();
-    }
-  }
-
-  loadPicture = () => {
-    const { pictureName, page } = this.state;
-    this.setState({ status: 'pending' });
+    
+    setStatus('pending');
     api
       .fetchPicture(pictureName, page)
       .then(res => {
-        this.setState(prevState => ({
-          pictureData: [...prevState.pictureData, ...mapper(res.data.hits)],
-          status: 'resolved',
-          IsLoadingMore:
-            prevState.pictureData.length + res.data.hits.length ===
-            res.data.totalHits
-              ? false
-              : true,
-        }));
+        setPictureData(state => [...state, ...mapper(res.data.hits)]);
+        setStatus('resolved');
         if (res.data.hits.length === 0) {
           toast.error('There is no picture for that name');
         }
       })
       .catch(error => console.log(error));
-  };
+  }, [page, pictureName]);
 
-  handleFormSubmit = pictureName => {
+const handleFormSubmit = pictureName => {
     // перезапись на новые 12 картинок при вводе новой строки валидной
-    this.resetPage();
-    this.setState({ pictureName });
+    setPage(1);
+  setPictureName({ pictureName });
+  setPictureData('');
   };
 
   // функция загрузки новых 12 картинок
-  loadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const loadMore = () => {
+    setPage(prevState => 
+      prevState.page + 1)
+    setIsLoadingMore('true')
   };
 
-  pictureModalClick = picture => {
-    this.setState({
-      pictureModal: picture,
-    });
+  const pictureModalClick = picture => {
+      setPictureModal(picture)
   };
 
   // 1-МОДАЛКА)метод для закрытия модалки-пик
-  closeModal = () => {
-    this.setState({
-      pictureModal: '',
-    });
+  const closeModal = () => {
+    setPictureModal('')
   };
 
   // скидываем страницу на 1 при новой валидной строки
-  resetPage() {
-    this.setState({
-      page: 1,
-    });
-  }
+  // resetPage() {
+  //   this.setState({
+  //     page: 1,
+  //   });
+  // }
 
-  // скидываем инпут поиска на 0
-  resetData() {
-    this.setState({
-      pictureData: '',
-      IsLoadingMore: false,
-    });
-  }
+  // // скидываем инпут поиска на 0
+  // resetData() {
+  //   this.setState({
+  //     pictureData: '',
+  //     IsLoadingMore: false,
+  //   });
+  // }
 
-  render() {
-    const { status, pictureData, pictureModal, IsLoadingMore } = this.state;
-    return (
-      <div>
-        <Searchbar onSubmit={this.handleFormSubmit} />
-        {pictureData.length > 0 && (
-          <ImageGallery
-            pictureData={pictureData}
-            onClick={this.pictureModalClick}
-          ></ImageGallery>
-        )}
-        {status === 'pending' && <LoaderSpiner />}
-        {IsLoadingMore && <LoadMore onClick={this.loadMore} />}
-        {pictureModal.length > 0 && (
-          <Modal onClose={this.closeModal}>
-            {/* 2-МОДАЛКА) кинули метод закрытия в пропс в модалку-пик */}
-            <img src={pictureModal} alt="" />
-          </Modal>
-        )}
-      </div>
-    );
+    // const { status, pictureData, pictureModal, IsLoadingMore } = this.state;
+  return (
+    <div>
+      <Searchbar onSubmit={handleFormSubmit} />
+      {pictureData.length > 0 && (
+        <ImageGallery
+          pictureData={pictureData}
+          onClick={pictureModalClick}
+        ></ImageGallery>
+      )}
+      {status === 'pending' && <LoaderSpiner />}
+      {IsLoadingMore && <LoadMore onClick={loadMore} />}
+      {pictureModal.length > 0 && (
+        <Modal onClose={closeModal}>
+          <img src={pictureModal} alt="" />
+        </Modal>
+      )}
+    </div>
+  );
   }
-}
